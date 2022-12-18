9f54748a7eca8c6c8ce743e160a79f399b9a6acf
neoking89 <75142390+neoking89@users.noreply.github.com>
2022-12-18 15:57:28 +0100

    added indexer similiar to Board class for making equality checks, added ToString method.

--- Piece.cs
+++ Piece.cs
@@ -6,10 +6,36 @@
 	public class Piece
     {
         public Color Color { get; set; }
-        public Piece(Color color)
+        public (int, int)? Position { get; set; }
+        public bool InPlay
+        {
+            get
+            {
+                return (Position != null);
+
+            }
+        }
+
+        public Piece(Color color, (int, int) position)
         {
             Color = color;
+            Position = position;
         }
+
+        public override string ToString()
+        {
+            var color = Color == Color.White ? "W" : "B";
+            return $"{color}, {Position}";
+        }
+
+        public (int, int)? this[int y, int x]
+        {
+            get
+            {
+                return Position;
+            }
+        }
+
 
     }
 }
