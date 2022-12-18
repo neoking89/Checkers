4c619160f717ddd123bb0981d012ec4b25b9c0f8
neoking89 <75142390+neoking89@users.noreply.github.com>
2022-12-18 15:56:25 +0100

    added properties to move class

--- Move.cs
+++ Move.cs
@@ -8,5 +8,12 @@ namespace Checkers;
 
 internal class Move
 {
-    
+    public (int, int) From { get; set; }
+    public (int, int) To { get; set; }
+    public Move((int, int) from, (int, int) to)
+    {
+        From = from;
+        To = to;
+    }
+
 }
