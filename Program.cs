e093c3c0704e7c031af7d651646b071efb0c7695
neoking89 <75142390+neoking89@users.noreply.github.com>
2022-12-18 15:57:45 +0100

    print pieces on board

--- Program.cs
+++ Program.cs
@@ -4,17 +4,14 @@ using System.Diagnostics;
 using System.Windows;
 
 
-//var myWriter = new TextWriterTraceListener(System.Console.Out);
-//Debug.Listeners.Add(myWriter);
-//Debug.
 
 Board board = new();
 board.CreatePieces();
-
-
-
-foreach (var item in board.CurrentPiecePositions)
+foreach (var piece in board.CurrentPiecePositions)
 {
-	Console.WriteLine(item.Key.Item1 + " " + item.Key.Item2 + " " + item.Value.Color);
-	
+    Console.WriteLine(piece.Value);
 }
+
+
+
+
